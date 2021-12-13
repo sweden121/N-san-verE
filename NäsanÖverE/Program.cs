5eef715b396e28e3e50e86170c3dbb691c612d3d
adriano mesquita <sweden1244@gmail.com>
2021-12-13 15:32:50 +0100

    vidiogenomgångar

--- NäsanÖverE/Program.cs
+++ NäsanÖverE/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
 
-namespace NäsanÖverE
+//int i = 0;
+//while(i<=5)
+//{
+//Console.WriteLine(i);        // denna kod för att räkna up till 5
+//i++;
+//}
+
+//Console.ReadLine();
+
+
+/*
+int i = 10;
+bool fråga = false;
+while (fråga==false)
 {
-    class Program
+    Console.WriteLine(i);         // räkna ner till 0
+    i = i - 1;
+    if (i<0)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        fråga = true;
     }
+    Console.ReadLine();
+
 }
+*/
+
+/*
+for (int i = 0; i < 10; i++)
+{
+    
+    Console.WriteLine(i);    // for loop är bättre då den är enklare men gör samma sak räknar ner 
+    Console.ReadLine();
+    
+}
+
+Console.ReadLine();
+*/
+
+/*
+int i = 4;
+int p = 3;
+if (i == 4 &&p==3)
+{
+Console.WriteLine("dedu");
+Console.WriteLine("dedudu");       // en if sats  lika med mindre högre
+
+}
+
+Console.ReadLine(); 
+
+*/
+
+
+
+/*
+string text = "värde";
+bool flint = false;
+bool röd = true;
+bool brun = true;
+if (text=="värde"&&flint==false)
+{
+
+    if (röd==true)
+    {
+        Console.WriteLine("du är röd");                  //  en if sats med flera steg 
+    }
+    else if (brun)
+    Console.WriteLine("du är brun");
+   else
+        Console.WriteLine("du är inte röd");
+
+}
+
+Console.ReadLine();
+
+*/
+
+
+
+
